9dad6e7104b71eb3eb7598db1332b964267d95b6
hardik8127 <hardikkumar159515@gmail.com>
2025-05-02 16:39:33 +0530

    Create Problem Functionality Added

--- backend/src/controllers/problem.controller.js
+++ backend/src/controllers/problem.controller.js
@@ -1,20 +1,81 @@
-const createProblem = async (req, res) => {};
+import { db } from "../libs/db.js";
+import { pollBatchResults, submitBatch } from "../libs/judge0.lib.js";
 
-const getAllProblems = async (req, res) => {};
+export const createProblem = async (req, res) => {
+  //  get data from the request body
+  const {
+    title,
+    description,
+    difficulty,
+    tags,
+    examples,
+    constraints,
+    testcases,
+    codesnippets,
+    referenceSolution,
+  } = req.body;
 
-const getProblemById = async (req, res) => {};
+  try {
+    for (const [language, solutionCode] of Object.entries(referenceSolution)) {
+      const languageId = getJudge0LanguageId(language);
 
-const updateProblem = async (req, res) => {};
+      if (!languageId) {
+        return res
+          .status(500)
+          .json({ error: `langauge ${language} is not supported` });
+      }
 
-const deleteProblem = async (req, res) => {};
+      const submissions = testcases.map(({ input, output }) => ({
+        source_code: solutionCode,
+        langauge_id: languageId,
+        stdin: input,
+        expected_output: output,
+      }));
 
-const getAllProblemsSolvedByUser = async (req, res) => {};
+      const submissionResults = await submitBatch(submissions);
 
-export {
-  createProblem,
-  getAllProblems,
-  getProblemById,
-  updateProblem,
-  deleteProblem,
-  getAllProblemsSolvedByUser,
+      const tokens = submissionResults.map((res) => res.token);
+
+      const results = await pollBatchResults(tokens);
+
+      for (let i = 0; i < results.length; i++) {
+        const result = results[i];
+
+        if (result.status.id !== 3) {
+          return res.status(400).json({
+            error: `Testcase ${i + 1} failed for language ${language}`,
+          });
+        }
+      }
+      const newProblem = await db.problem.create({
+        data: {
+          title,
+          description,
+          difficulty,
+          tags,
+          examples,
+          constraints,
+          testcases,
+          codesnippets,
+          referenceSolution,
+          userId: req.userId,
+        },
+      });
+      return res.status(201).json(newProblem);
+    }
+  } catch (error) {}
+  // check if the user is an admin
+  // loop through earch refence solution
+  // get language id for current language
+  // prepare judge0  submission
 };
+
+export const getAllProblems = async (req, res) => {};
+
+export const getProblemById = async (req, res) => {};
+
+export const updateProblem = async (req, res) => {};
+
+export const deleteProblem = async (req, res) => {};
+
+export const getAllProblemsSolvedByUser = async (req, res) => {};
