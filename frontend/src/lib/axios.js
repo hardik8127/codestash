91e97d2a6214206c9dc08168937cf5d5ef143494
hardik8127 <hardikkumar159515@gmail.com>
2025-07-18 12:08:08 +0530

    misc fixes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "https://codestash-backend-d2fc20e3f35a.herokuapp.com/api/v1",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:8080/api/v1" : "https://codestash-backend-d2fc20e3f35a.herokuapp.com/api/v1",
     withCredentials: true,
 })
